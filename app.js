5be18673d1e5e3dfd5ae4448a6922a9885c14d8a
Atwan-Sha <atwanshaaban@gmail.com>
2023-02-27 21:07:57 +0100

    added hamburger nav menu

---
+++ app.js
@@ -0,0 +1,13 @@
+// * nav hamburger menu
+const nav = document.querySelector("nav");
+const icon = document.querySelector(".icon");
+const menu = document.querySelector(".expand");
+
+icon.addEventListener("mouseenter", () => {
+    menu.style.display = "inline";
+});
+
+nav.addEventListener("mouseleave", () => {
+    menu.style.display = "none";
+})
+
