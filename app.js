0a5e4da5b67feca91a287b2d862a6c2c353a9bc0
Atwan-Sha <atwanshaaban@gmail.com>
2023-05-27 06:02:08 +0200

    finish animated hamburger menu

--- app.js
+++ app.js
@@ -1,17 +1,31 @@
 const bgText = document.querySelector(".bg-text");
 const nav = document.querySelector("nav");
-const icon = document.querySelector(".icon");
-const menu = document.querySelector(".expand");
-
 const body = document.querySelector("body");
 const bgWrapper = document.querySelector(".bg-wrapper");
 const fgWrapper = document.querySelector(".fg-wrapper");
-// console.log(bgWrapper, fgWrapper);
+const menuButtonContainer = document.querySelector(".menu-button-container");
 
 
+// *bg ascii secret message
 let bgTextStr = '010010010010000001100001011011010010000001100001011011100010000001100001011101110110010101110011011011110110110101100101001000000110010001100101011101100110010101101100011011110111000001100101011100100010000100100000010100000110110001100101011000010111001101100101001000000110100001101001011100100110010100100000011011010110010100100001001000000011110100101001';
 bgTextStr = bgTextStr.repeat(15).split('').join(' ');
 bgText.insertAdjacentText('beforeEnd', bgTextStr);
+
+
+// ! add front layer for fade
+let menuClicked = false;
+menuButtonContainer.addEventListener('click', () => {
+    if(!menuClicked){
+        bgWrapper.classList.add('fade');
+        menuClicked = true;
+    }else{
+        bgWrapper.classList.remove('fade');
+        menuClicked = false;
+    }
+});
+
+
+
 
 // // * nav hb menu
 // icon.addEventListener("mouseenter", () => {
@@ -33,6 +47,8 @@ bgText.insertAdjacentText('beforeEnd', bgTextStr);
 //         // console.log('remove sticky');
 //     }
 // });
+
+
 
 
 
